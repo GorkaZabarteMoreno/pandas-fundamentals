8d4dc12342e4c2febc332c79a73e5b490f6177d0
GorkaZabarteMoreno <gorka.zabarte.moreno@alumnos.upm.es>
2022-12-08 20:19:56 +0100

    main.py file as the entry point of the application
    Create main.py file to establish it as the main file of
    the application.

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+"""
+main file of the project
+"""
 import pandas as pd
 
+
 if __name__ == '__main__':
+    app_name: str = 'Gorka'
+    print(f'{app_name} application')
+
     data = pd.read_csv(filepath_or_buffer="./data/grades.csv", delimiter=",", header="infer")
     print(data.head())
